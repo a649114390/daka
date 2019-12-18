3e8abc42ee70bbad6543afc7ecfb91c8d1bf5a5d
geek <649114390@qq.com>
2019-12-18 23:30:35 +0800

    feat(打卡):完成开发

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -2,6 +2,7 @@
 namespace app\index\controller;
 
 use app\index\service\DaKaService;
+use app\index\service\RiBaoService;
 use think\Db;
 use think\Validate;
 
@@ -43,6 +44,30 @@ class Index
     public function daka() {
         $service = new DaKaService();
         $service->run();
+
+    }
+    public function ribao() {
+
+        $data = [
+            'msgtype' => 'text',
+            'text'    => [
+                'content' => '汇报日报了各位大佬(已写的请忽略)'
+            ]
+        ];
+        $date = date('Y-m-d');
+        var_dump($date);
+        $hm = date('H:i');
+        $w = date('w', strtotime($date));
+        if($w==6 || $w == 0){
+            var_dump($date . '周末，跳过');
+            die();
+        }
+        if ($hm !== '22:30') {
+            var_dump('不是期望时间不提示');
+            die();
+        }
+        $riBao = new RiBaoService();
+        $riBao->sendMsg($data);
 
     }
 }
